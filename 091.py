7514ca6843dc9d97718450f25e2d900da9a79b17
Henrique Lopes <oprometeumoderno@gmail.com>
2026-06-18 15:30:23 -0300

    make 091 faster

--- 091.py
+++ 091.py
@@ -13,12 +13,16 @@ def is_rectangle(x0, y0, x1, y1):
 
 
 def solution091():
+    same_x = 0
     total = 0
     LIMIT = 50
     for x0 in range(0, LIMIT + 1):
-        for x1 in range(0, LIMIT + 1):
-            for y0 in range(0, LIMIT + 1):
+        for y0 in range(0, LIMIT + 1):
+            for x1 in range(0, x0 + 1):
                 for y1 in range(0, LIMIT + 1):
                     if is_rectangle(x0, y0, x1, y1):
                         total += 1
-    return int(total / 2)
+                        if x0 == x1:
+                            same_x += 1
+
+    return int(total - int(same_x / 2))
