2fdc0e9ee714653bbc5fe351bb6b87661da4b9cf
Henrique Lopes <oprometeumoderno@gmail.com>
2026-06-25 18:37:30 -0300

    problem 97 speedup

--- 097.py
+++ 097.py
@@ -1,8 +1,8 @@
 def solution097():
     n = 1
-    for i in range(7830457):
-        n *= 2
+    for i in range(783045):
+        n *= 1024
         if n > 10**12:
             n = n % 10**11
 
-    return (n * 28433 + 1) % 10**10
+    return ((n * 128) * 28433 + 1) % 10**10
